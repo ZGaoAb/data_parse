1da993a4dfb98226ae3b16889a092894b6ecaefe
ZhengGao <zgaohands@gmail.com>
2021-03-28 23:20:22 +0100

    some tests..

--- parse_work/tests.py
+++ parse_work/tests.py
@@ -1,2 +1,10 @@
 from django.test import TestCase
 
+from parse_work.models import Temperature
+
+class Demo(TestCase):
+    datas = Temperature.objects.all()
+    for data in datas:
+        print(data)
+
+
